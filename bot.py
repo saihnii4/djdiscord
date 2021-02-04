eb3bd75bc735e2b45b44f4abfbf49448047c8b81
nomadiccode <n0mad1cc0d3@gmail.com>
2021-02-04 19:42:05 +0800

    some stuff

--- bot.py
+++ bot.py
@@ -7,11 +7,15 @@ from utils.extensions import DJDiscord
 
 dotenv.load_dotenv()
 
-bot = DJDiscord(command_prefix=os.environ["BOT_PREFIX"],
-                intents=discord.Intents(guild_messages=True,
-                                        voice_states=True,
-                                        guilds=True,
-                                        guild_reactions=True,
-                                        reactions=True,
-                                        typing=True))
+bot = DJDiscord(
+    command_prefix=os.environ["BOT_PREFIX"],
+    intents=discord.Intents(
+        guild_messages=True,
+        voice_states=True,
+        guilds=True,
+        guild_reactions=True,
+        reactions=True,
+        typing=True,
+    ),
+)
 bot.run(os.environ["BOT_TOKEN"])
--- utils/extensions.py
+++ utils/extensions.py
@@ -22,7 +22,8 @@ class DJDiscordContext(discord.ext.commands.Context):
     def player(self: DJDiscordContext) -> None:
         if not self.bot.lavalink.player_manager.get(self.guild.id):
             player = self.bot.lavalink.player_manager.create(
-                self.guild.id, endpoint=str(self.guild.region))
+                self.guild.id, endpoint=str(self.guild.region)
+            )
             return player
         return self.bot.lavalink.player_manager.get(self.guild.id)
 
@@ -33,8 +34,8 @@ class DJDiscordContext(discord.ext.commands.Context):
     @property
     async def dj(self: DJDiscordContext):
         role_id = await self.database.psqlconn.fetch(
-            """SELECT (dj_role) FROM configuration WHERE id=$1""",
-            self.guild.id)
+            """SELECT (dj_role) FROM configuration WHERE id=$1""", self.guild.id
+        )
 
         role = discord.utils.get(self.guild.roles, id=role_id)
 
@@ -53,60 +54,108 @@ class DJDiscordContext(discord.ext.commands.Context):
 
 class DJDiscord(discord.ext.commands.Bot):
     """DJDiscord [discord.ext.commands.Bot] -> Base class for DJ Discord"""
+
     def __init__(self, *args, **kwargs):
-        super().__init__(*args,
-                         **kwargs,
-                         help_command=PrettyHelp(
-                             dm_help=False,
-                             color=0xDC333C,
-                             no_category="General Commands",
-                             index_title="DJDiscord Commands",
-                             show_index=False))
+        super().__init__(
+            *args,
+            **kwargs,
+            help_command=PrettyHelp(
+                dm_help=False,
+                color=0xDC333C,
+                no_category="General Commands",
+                index_title="DJDiscord Commands",
+                show_index=False,
+            )
+        )
         self.voice_queue = {}
         for object in os.listdir("./commands"):
-            if os.path.isfile("./commands/%s" % object) and os.path.splitext(
-                    "./commands/%s" % object)[1] == ".py":
-                self.load_extension("commands.%s" %
-                                    os.path.splitext(object)[0])
+            if (
+                os.path.isfile("./commands/%s" % object)
+                and os.path.splitext("./commands/%s" % object)[1] == ".py"
+            ):
+                self.load_extension("commands.%s" % os.path.splitext(object)[0])
         self.load_extension("jishaku")
         self.loop.create_task(self.update_presence())
 
     async def update_presence(self) -> None:
         await self.wait_until_ready()
-        await self.change_presence(activity=discord.Activity(
-            type=discord.ActivityType.competing,
-            name="{} server{}. Prefix: dj;".format(
-                len(self.guilds), "" if len(self.guilds) == 1 else "s")))
+        await self.change_presence(
+            activity=discord.Activity(
+                type=discord.ActivityType.competing,
+                name="{} server{}. Prefix: dj;".format(
+                    len(self.guilds), "" if len(self.guilds) == 1 else "s"
+                ),
+            )
+        )
         await asyncio.sleep(120)
 
     async def on_connect(self):
         self.lavalink = lavalink.Client(self.user.id)
-        self.lavalink.add_node(os.environ["LAVALINK_HOST"],
-                               os.environ["LAVALINK_PORT"],
-                               os.environ["LAVALINK_PASSWORD"],
-                               os.environ["LAVALINK_REGION"],
-                               os.environ["LAVALINK_NODE_NAME"])
-        self.add_listener(self.lavalink.voice_update_handler,
-                          'on_socket_response')
-        rethinkdb.r.set_loop_type('asyncio')
+        self.lavalink.add_node(
+            os.environ["LAVALINK_HOST"],
+            os.environ["LAVALINK_PORT"],
+            os.environ["LAVALINK_PASSWORD"],
+            os.environ["LAVALINK_REGION"],
+            os.environ["LAVALINK_NODE_NAME"],
+        )
+        self.add_listener(self.lavalink.voice_update_handler, "on_socket_response")
+        rethinkdb.r.set_loop_type("asyncio")
+        self._admin_rdbconn = await rethinkdb.r.connect()
+        if (
+            not await rethinkdb.r.db("rethinkdb")
+            .table("test")
+            .get("djdiscord")
+            .run(self._admin_rdbconn)
+        ):
+            await rethinkdb.r.db("rethinkdb").table("test").insert(
+                {"id": "djdiscord", "password": os.environ["RETHINKDB_PASSWORD"]}
+            ).run(self._admin_rdbconn)
+
+            if "djdiscord" not in await rethinkdb.r.db_list().run(self._admin_rdbconn):
+                await rethinkdb.r.db_create("djdiscord").run(self._admin_rdbconn)
+
+            if "playlists" not in await rethinkdb.r.db("djdiscord").table_list().run(
+                self._admin_rdbconn
+            ):
+                await rethinkdb.r.db("djdiscord").table_create("playlists").run(
+                    self._admin_rdbconn
+                )
+
+            if "stations" not in await rethinkdb.r.db("djdiscord").table_list().run(
+                self._admin_rdbconn
+            ):
+                await rethinkdb.r.db("djdiscord").table_create("stations").run(
+                    self._admin_rdbconn
+                )
+
+            if "logs" not in await rethinkdb.r.db("djdiscord").table_list().run(
+                self._admin_rdbconn
+            ):
+                await rethinkdb.r.db("djdiscord").table_create("logs").run(
+                    self._admin_rdbconn
+                )
+        del self._admin_rdbconn
         self.rdbconn = await rethinkdb.r.connect(
             db="djdiscord",
             host=os.environ["RETHINKDB_HOST"],
             port=os.environ["RETHINKDB_PORT"],
             user=os.environ["RETHINKDB_USERNAME"],
-            password=os.environ["RETHINKDB_PASSWORD"])
+            password=os.environ["RETHINKDB_PASSWORD"],
+        )
         self.psqlconn = await asyncpg.connect(
             user=os.environ["POSTGRESQL_USERNAME"],
             password=os.environ["POSTGRESQL_PASSWORD"],
             database="djdiscord_config",
             host=os.environ["POSTGRESQL_HOST"],
-            port=os.environ["POSTGRESQL_PORT"])
+            port=os.environ["POSTGRESQL_PORT"],
+        )
 
     async def on_ready(self):
         print("Ready!")
 
-    async def process_commands(self: discord.ext.commands.Bot,
-                               message: discord.Message) -> None:
+    async def process_commands(
+        self: discord.ext.commands.Bot, message: discord.Message
+    ) -> None:
         if message.author.bot:
             return
 
