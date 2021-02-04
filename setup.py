f0caba75e05aaaebc092e3efb711ae3fef29c3bf
nomadiccode <n0mad1cc0d3@gmail.com>
2021-02-04 18:47:05 +0800

    added lavalink installation

--- setup.py
+++ setup.py
@@ -3,8 +3,23 @@ import os
 
 if "progress.png" not in os.listdir("./assets"):
     with open("./assets/progress.png", "wb") as progress:
-        progress.write(requests.get("https://notduckduckcode.github.io/djdiscord/assets/progress.png").content)
+        progress.write(
+            requests.get(
+                "https://notduckduckcode.github.io/djdiscord/assets/progress.png"
+            ).content
+        )
 
 if "logo.png" not in os.listdir("./assets"):
     with open("./assets/logo.png", "wb") as logo:
-        logo.write(requests.get("https://notduckduckcode.github.io/djdiscord/assets/logo.png").content)
+        logo.write(
+            requests.get(
+                "https://notduckduckcode.github.io/djdiscord/assets/logo.png"
+            ).content
+        )
+
+with open("./Lavalink.jar", "wb") as lavalink:
+    lavalink.write(
+        requests.get(
+            "https://github.com/Frederikam/Lavalink/releases/download/3.3.2.3/Lavalink.jar"
+        ).content
+    )
